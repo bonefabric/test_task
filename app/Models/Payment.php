4fde812f865bde69c7870af7d981fbd636c3bc76
iburangulov <iburanguloff@ya.ru>
2021-10-28 06:58:00 +0500

    payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Payment extends Model
+{
+	use HasFactory, SoftDeletes;
+
+	protected $fillable = [
+		'sum',
+	];
+
+	/**
+	 * @return HasOne
+	 */
+	public function user(): HasOne
+	{
+		return $this->hasOne(User::class, 'user_ref');
+	}
+}
